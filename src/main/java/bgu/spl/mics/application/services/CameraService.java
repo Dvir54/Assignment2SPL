9ae1eb04071fbbc712907b31b63dc18c55f915b9
Dvir54 <dvir54693@gmail.com>
2024-12-31 14:08:16 +0200

    lasttttt

--- src/main/java/bgu/spl/mics/application/services/CameraService.java
+++ src/main/java/bgu/spl/mics/application/services/CameraService.java
@@ -44,25 +44,27 @@ public class CameraService extends MicroService {
     protected void initialize() {
         subscribeBroadcast(TickBroadcast.class, (TickBroadcast tick) ->{
             int currenTime = tick.getCurrentTime();
+            //check if the camera is up, otherwise the camera is Error and the every other service should terminate
             if (camera.getStatus() == Camera.Status.UP) {
-                //when the camera may have objects to detect at that specific tick
+                //check if the current time is within the range of the camera's detected objects, otherwise the camera is down
                 if(currenTime <= camera.getDetectedObjectsList().get(camera.getDetectedObjectsList().size()-1).getTime()){
-                    for(StampedDetectedObjects detectedObjects : camera.getDetectedObjectsList()){
-                        if(detectedObjects.getTime() + camera.getFrequency() == currenTime){
-                            //check if I have an object with ID error
-                            String description = detectedObjects.checkIfError();
-                            if(description != null){
-                                camera.setStatus(Camera.Status.ERROR);
-                                sendBroadcast(new CrashedBroadcast(description));
-                                terminate();
-                            }
-                            else{
-                                DetectObjectsEvent detectObjectsEvent = new DetectObjectsEvent(detectedObjects);
-                                statisticalFolder.incrementDetectedObjects(detectedObjects.getDetectedObjectsList().size());
-                                sendEvent(detectObjectsEvent);
-                            }
+                    StampedDetectedObjects stampedDetectedObjects = camera.getStampedDetectedObject(currenTime);
+                    //check if the camera detects objects at that tick, otherwise do nothing
+                    if (stampedDetectedObjects != null && !stampedDetectedObjects.getDetectedObjectsList().isEmpty()) {
+                        String description = camera.checkError(stampedDetectedObjects);
+                        //check if the camera detected an error, otherwise send the detected objects
+                        if( description != null){
+                            camera.setStatus(Camera.Status.ERROR);
+                            sendBroadcast(new CrashedBroadcast(description));
+                            terminate();
+                        }
+                        else{
+                            DetectObjectsEvent detectObjectsEvent = new DetectObjectsEvent(stampedDetectedObjects);
+                            statisticalFolder.incrementDetectedObjects(stampedDetectedObjects.getDetectedObjectsList().size());
+                            sendEvent(detectObjectsEvent);
                         }
                     }
+
                 }
                 else {
                     camera.setStatus(Camera.Status.DOWN);
